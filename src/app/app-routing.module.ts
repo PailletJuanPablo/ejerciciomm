b2fe1c08331937f0321de9a2f9ae7e1542b8e3c0
Juan Pablo Paillet <pailletjuanpablo@gmail.com>
2019-08-30 18:56:52 -0300

    Se agrego el componente pie de pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { InicioComponent } from './inicio/inicio.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { ServiciosComponent } from './servicios/servicios.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 // {path: 'ruta', component: ClaseComponent }
 const routes: Routes = [
